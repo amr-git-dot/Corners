f57254c7bc1e15c87ff7e456e2a450f0f5f84f28
Spider0x <74156139+amr-git-dot@users.noreply.github.com>
2023-05-13 03:17:29 +0300

    Add files via upload

---
+++ Corners.ps1
@@ -0,0 +1,153 @@
+$ErrorActionPreference = "SilentlyContinue"
+
+Write-Output "Persist Plugin Running..."
+
+function helpmsg { 
+    write-output "help message"
+    Write-Host "####### This plugin is used To check for a big number of persistence places where malware can laverage #######"
+    write-output "no arguments needed"
+    }
+
+if ($args -contains "-h") {
+	helpmsg
+	return
+}
+
+$currentDate = Get-Date   -Format "yyyyMMdd_HHmm"
+
+$result = Join-Path -Path "." -ChildPath "Persist_$currentDate.txt"
+
+$admin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+$seperator = "`n###################################################`n"
+
+
+function print {
+    param (
+        [string]$Header,
+        [Object]$data, 
+		[string]$tech
+    )
+    
+Out-File -FilePath $result -InputObject $Header -Encoding ASCII -Append
+Out-File -FilePath $result -InputObject $tech -Encoding ASCII -Append
+Out-File -FilePath $result -InputObject $seperator -Encoding ASCII -Append
+Out-File -FilePath $result -InputObject $data -Encoding ASCII -Append
+Out-File -FilePath $result -InputObject $seperator -Encoding ASCII -Append
+
+}
+
+
+
+# Registry Keys
+$file = ".\artifacts\regkeys_persist.txt"
+
+foreach ($key in Get-Content $file){
+	$reg_key , $tech = $key.Split('-').Trim()
+    $value = Get-ItemProperty -Path $reg_key
+    print -Header $reg_key -data $value -tech $tech
+}
+
+# Registry values
+$file = ".\artifacts\regvalues_persist.txt"
+
+foreach ($key in Get-Content $file){
+    
+
+
+    $path, $valueName, $tech = $key.Split('-').Trim()
+	$value =  Get-ItemProperty -Path $path -Name $valueName
+	$Header = "$path`n$valueName"
+    
+	print -Header $Header -data $value.$valueName -tech $tech
+}
+
+# Startup folders Content
+$tech = "Mittre Technique number T1547.001"
+# Get the path to the current user's startup folder
+$userStartupFolder = [Environment]::GetFolderPath('Startup')
+
+# Get the path to the all users' startup folder
+$allUsersStartupFolder = [Environment]::GetFolderPath('CommonStartup')
+
+# Get the file names in the current user's startup folder
+$userStartupFiles = Get-ChildItem -Path $userStartupFolder | Select-Object -ExpandProperty Name
+
+# Get the file names in the all users' startup folder
+$allUsersStartupFiles = Get-ChildItem -Path $allUsersStartupFolder | Select-Object -ExpandProperty Name
+
+# Print the file names in both startup folders
+	
+
+	print -Header "Current user's startup folder files:" -data $userStartupFiles -tech $tech
+
+	print -Header "All users' startup folder files:" -data $allUsersStartupFiles -tech $tech
+
+# BITS job investigation
+$tech = "Mittre Technique number T1197"
+if (!$admin) {
+	$res = Get-BitsTransfer | Select-Object -Property *
+    	print -Header "BITS jobs:" -data $res -tech $tech
+}else{
+    $res = Get-BitsTransfer -AllUsers | Where-Object { $_.JobState -ne "Transferred" } | Select-Object -Property *
+	  	print -Header "BITS jobs:" -data $res -tech $tech
+
+}
+
+# Active setup
+
+$key = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\"
+$tech = "Mittre Technique number T1547.014"
+$keys = Get-ChildItem -Path $key
+$Header = "Active SetUp"
+	Out-File -FilePath $result -InputObject $Header -Encoding ASCII -Append
+
+foreach ($item in $keys){
+    $path_ = $item.name
+    $path_ = $path_.Substring($path_.IndexOf("HKEY_LOCAL_MACHINE"))
+    $path = $path_.Replace("HKEY_LOCAL_MACHINE", "HKLM:")   
+	$value =  Get-ItemProperty -Path $path
+	$value_ = $value.StubPath
+	if ($value_) {
+		$res = "$path`n$value_"
+    	print -Header "" -data $res -tech $tech
+	}
+	
+
+}
+
+$res = gci -path "C:\Users\" -recurse -include *.lnk -ea SilentlyContinue | Select-String -Pattern "exe" | FL
+$tech = "Mittre Technique number T1547.009"
+print -Header "LNK Modification" -data $res -tech $tech
+
+$key = "HKLM:\SYSTEM\CurrentControlSet\Services"
+$tech = "Mittre Technique number T1543.003"
+$res = ""
+$entries = Get-ChildItem $key
+
+foreach($entry in $entries){
+	$path_ = $entry.name
+    $path_ = $path_.Substring($path_.IndexOf("HKEY_LOCAL_MACHINE"))
+    $path = $path_.Replace("HKEY_LOCAL_MACHINE", "HKLM:") 
+	$prop = Get-ItemProperty $path 
+	$name = $entry.Name
+	$image = $prop.ImagePath
+	$res += "$name`n_$image`n`n"
+}
+
+print -Header "Services and pathes" -data $res -tech $tech
+
+$res = Get-WmiObject -Namespace root\subscription -Class __FilterToConsumerBinding
+$tech = "Mittre Technique number T1546.003"
+
+print -Header "WMI Subscription" -data $res -tech $tech
+
+$res = $env:Path -split ';'
+$tech = "Mittre Technique number T1574.007"
+print -Header "Path variable (Hijacking techniques)" -data $res -tech $tech
+
+$res = $env:COR_PROFILER
+$tech = "Mittre Technique number T1574.012"
+
+print ".Net CLR Libraries" -data $res -tech $tech
+
---
+++ Corners_baseline.ps1
@@ -0,0 +1,97 @@
+# Store the current value of $ErrorActionPreference
+$oldErrorActionPreference = $ErrorActionPreference
+
+# Set $ErrorActionPreference to "SilentlyContinue"
+$ErrorActionPreference = "SilentlyContinue"
+
+[CmdletBinding()]
+    param (
+        [String]$target,
+        [String]$baseline,
+        [string]$key
+    )
+
+
+
+function helpmsg {
+    write-output "help message"
+    Write-Host "####### This plugin is used To compare an persist plugin output from a suspected machine and a baseline one #######"
+    write-output "-target path of the suspected"
+    write-output "-baseline path of the baseline"
+}
+
+if ($args -contains "-h") {
+    helpmsg
+    return  
+  }
+
+#Check for existence of HASH or PATH
+if (!($target)) {
+    Write-Host ""
+    Write-Host -ForegroundColor Yellow 'Missing the target file PATH, you must supply one. -target'
+    Write-Host ""
+    exit
+}
+
+if (!($baseline)) {
+    Write-Host ""
+    Write-Host -ForegroundColor Yellow 'Missing the baseline file PATH, you must supply one. -baseline'
+    Write-Host ""
+    exit
+}
+
+Write-Output "Persist_baseline Plugin Running(the green is in your baseline)...`n"
+
+$file2 = (get-content $target)
+$file1 = (get-content $baseline)
+
+
+$differences = compare-object $file1 $file2
+
+foreach ($difference in $differences) {
+    if ($difference.InputObject -notmatch "_[0-9A-F]{5}$") {
+       
+        if ($difference.InputObject -notmatch "\{[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}\}") {
+            if ($difference.InputObject -in $file1) {
+                Write-Host $difference.InputObject -ForegroundColor Green
+            }
+            else {
+                Write-Host $difference.InputObject -ForegroundColor Red
+            }
+
+        }else{
+            $line = $difference.InputObject -replace "\{[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}\}",""
+            if ((Select-String -Path $target -Pattern $line -SimpleMatch) -ne $null -and (Select-String -Path $baseline -Pattern $line -SimpleMatch) -ne $null) {
+            
+            }
+            else {
+                if ($difference.InputObject -in $file1) {
+                    Write-Host $difference.InputObject -ForegroundColor Green
+                }
+                else {
+                    Write-Host $difference.InputObject -ForegroundColor Red
+                }
+                
+            }
+        }
+    
+        
+    }else{
+        $line = $difference.InputObject -replace "_[0-9A-F]{5}$",""
+        if ((Select-String -Path $target -Pattern $line -SimpleMatch) -ne $null -and (Select-String -Path $baseline -Pattern $line -SimpleMatch) -ne $null) {
+            
+        }
+        else {
+            if ($difference.InputObject -in $file1) {
+                Write-Host $difference.InputObject -ForegroundColor Green
+            }
+            else {
+                Write-Host $difference.InputObject -ForegroundColor Red
+            }
+            
+        }
+    }
+}
+
+# Restore the original value of $ErrorActionPreference
+$ErrorActionPreference = $oldErrorActionPreference
